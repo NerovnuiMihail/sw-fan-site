7a9c8b1d1bf6e326745a0ffee70c9a12f638f4da
NerovnuiMihail <79864794+NerovnuiMihail@users.noreply.github.com>
2021-09-21 16:53:41 +0300

    add gh-page

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 import routesConfig from "../../routes/routesConfig";
+import {REPO_NAME} from "../../constants/repo";
 import Header from "../../components/Header/Header";
 
 import styles from  "./App.module.css";
@@ -8,7 +9,7 @@ import styles from  "./App.module.css";
 const App = () => {
   return (
       <>
-          <BrowserRouter>
+          <BrowserRouter basename={`/${REPO_NAME}/`}>
               <div className={styles.wrapper}>
                   <Header />
 
